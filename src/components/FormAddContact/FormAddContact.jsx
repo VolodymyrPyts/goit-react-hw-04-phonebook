c05ba0d05a03b6211fa700adaee2ab1fe7bd6cf8
Volodymyr Pyts <volodymyr.pyts@gmail.com>
2022-09-03 16:21:17 +0300

    Form Huck

--- src/components/FormAddContact/FormAddContact.jsx
+++ src/components/FormAddContact/FormAddContact.jsx
@@ -1,75 +1,76 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
 import { LabelStyle, ButtonStyle } from './FormAddContact.styled';
 import { Box } from 'components/theme/Box';
-import PropTypes from 'prop-types';
-export class FormAddContact extends Component {
-  state = {
-    name: '',
-    number: '',
+
+export const FormAddContact = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  reset = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
-  };
-
-  onSubmit = e => {
-    const { name, number } = this.state;
-
+  const onHandleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(name, number);
-    this.reset();
+    onSubmit(name, number);
+    reset();
   };
 
-  onInputChange = e => {
+  const onInputChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        console.log('data error');
+    }
   };
 
-  render() {
-    const { name, number } = this.state;
-    const { onSubmit, onInputChange } = this;
+  return (
+    <Box width="400px">
+      <form onSubmit={onHandleSubmit}>
+        <LabelStyle>
+          <Box as="p" display="inline">
+            Name
+          </Box>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={onInputChange}
+          />
+        </LabelStyle>
+        <LabelStyle>
+          <Box as="p" display="inline" mr="auto">
+            Namber
+          </Box>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={onInputChange}
+          />
+        </LabelStyle>
+        <ButtonStyle type="submit">Add new Contact</ButtonStyle>
+      </form>
+    </Box>
+  );
+};
 
-    return (
-      <Box width="400px">
-        <form onSubmit={onSubmit}>
-          <LabelStyle>
-            <Box as="p" display="inline">
-              Name
-            </Box>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={name}
-              onChange={onInputChange}
-            />
-          </LabelStyle>
-          <LabelStyle>
-            <Box as="p" display="inline" mr="auto">
-              Namber
-            </Box>
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              value={number}
-              onChange={onInputChange}
-            />
-          </LabelStyle>
-          <ButtonStyle type="submit">Add new Contact</ButtonStyle>
-        </form>
-      </Box>
-    );
-  }
-}
+FormAddContact.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
